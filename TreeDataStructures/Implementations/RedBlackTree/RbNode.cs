c70b22df7f54c4cdc5ad259733636507400a50a2
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2026-04-25 20:30:08 +0300

    Add property "Sibling" for "RbNode"

--- TreeDataStructures/Implementations/RedBlackTree/RbNode.cs
+++ TreeDataStructures/Implementations/RedBlackTree/RbNode.cs
@@ -14,4 +14,5 @@ public class RbNode<TKey, TValue>(TKey key, TValue value)
     public RbColor Color { get; set; } = RbColor.Red;
     public RbNode<TKey, TValue>? Grandparent => this?.Parent?.Parent;
     public RbNode<TKey, TValue>? Uncle => this.IsLeftChild ? this.Grandparent?.Right : this.Grandparent?.Left;
+    public RbNode<TKey, TValue>? Sibling => this.IsLeftChild ? this.Parent?.Right : this.Parent?.Left;
 }
