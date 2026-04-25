5552a7e57c457758f3634f6dacfec6ee77d9b425
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2026-04-25 14:00:23 +0300

    Add method "UpdateHeight" in AVL

--- TreeDataStructures/Implementations/AVL/AvlTree.cs
+++ TreeDataStructures/Implementations/AVL/AvlTree.cs
@@ -8,6 +8,8 @@ public class AvlTree<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, AvlNode<
 {
     private int GetHeight(AvlNode<TKey, TValue>? node) => node?.Height ?? 0;
 
+    private int UpdateHeight(AvlNode<TKey, TValue> node) => Math.Max(GetHeight(node.Left), GetHeight(node.Right)) + 1;
+
     private int GetBalanceFactor(AvlNode<TKey, TValue> node) => GetHeight(node.Left) - GetHeight(node.Right);
 
     protected override AvlNode<TKey, TValue> CreateNode(TKey key, TValue value) => new(key, value);
