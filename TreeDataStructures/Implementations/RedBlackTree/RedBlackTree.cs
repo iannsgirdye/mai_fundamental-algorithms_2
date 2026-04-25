aceea231592ede70737b6b9898c259789b2c5d39
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2026-04-25 17:40:14 +0300

    Add method "IsBlack" for RB

--- TreeDataStructures/Implementations/RedBlackTree/RedBlackTree.cs
+++ TreeDataStructures/Implementations/RedBlackTree/RedBlackTree.cs
@@ -4,6 +4,8 @@ namespace TreeDataStructures.Implementations.RedBlackTree;
 
 public class RedBlackTree<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, RbNode<TKey, TValue>>
 {
+    private bool IsBlack(RbNode<TKey, TValue>? node) => node == null || node.Color == RbColor.Black;
+
     protected override RbNode<TKey, TValue> CreateNode(TKey key, TValue value) => new(key, value);
     
     protected override void OnNodeAdded(RbNode<TKey, TValue> newNode)
