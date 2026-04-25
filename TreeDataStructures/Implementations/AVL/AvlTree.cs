5b80647cc8b112768576eabc97d3d677ec18ab7c
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2026-04-25 13:41:51 +0300

    Add method "GetHeight" for AVL

--- TreeDataStructures/Implementations/AVL/AvlTree.cs
+++ TreeDataStructures/Implementations/AVL/AvlTree.cs
@@ -1,17 +1,21 @@
-﻿using TreeDataStructures.Core;
+﻿using System;
+using TreeDataStructures.Core;
 
 namespace TreeDataStructures.Implementations.AVL;
 
 public class AvlTree<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, AvlNode<TKey, TValue>>
     where TKey : IComparable<TKey>
     {
+
+    private int GetHeight(AvlNode<TKey, TValue>? node) => node != null ? node.Height : 0;
+
     protected override AvlNode<TKey, TValue> CreateNode(TKey key, TValue value)
         => new(key, value);
-    
+
     protected override void OnNodeAdded(AvlNode<TKey, TValue> newNode)
     {
         throw new NotImplementedException();
-    }
+            }
 
     protected override void OnNodeRemoved(AvlNode<TKey, TValue>? parent, AvlNode<TKey, TValue>? child) {
         throw new NotImplementedException();
