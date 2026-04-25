b99eaf9521bc40ecd29e9b166f18ad264a5daf75
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2026-04-25 12:14:21 +0300

    Fix formatting for "Treap" methods

--- TreeDataStructures/Implementations/Treap/Treap.cs
+++ TreeDataStructures/Implementations/Treap/Treap.cs
@@ -15,12 +15,15 @@ public class Treap<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, TreapNode<
         {
             return (null, null);
         }
-        if (Comparer.Compare(key, root.Key) <= 0) {
+        if (Comparer.Compare(key, root.Key) <= 0)
+        {
             (var newLeft, var newRight) = Split(root.Left, key);
             root.Left = newRight;
             root.Left?.Parent = root;
             return (newLeft, root);
-        } else {
+        }
+        else
+        {
             (var newLeft, var newRight) = Split(root.Right, key);
             root.Right = newLeft;
             root.Right?.Parent = root;
@@ -35,15 +38,18 @@ public class Treap<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, TreapNode<
     /// </summary>
     protected virtual TreapNode<TKey, TValue>? Merge(TreapNode<TKey, TValue>? left, TreapNode<TKey, TValue>? right)
     {
-        if (left == null || right == null) {
+        if (left == null || right == null)
+        {
             return left ?? right;
         }
-        if (left.Priority >= right.Priority) {
+        if (left.Priority >= right.Priority)
+        {
             left.Right = Merge(left.Right, right);
             left.Right?.Parent = left;
             return left;
         }
-        else {
+        else
+        {
             right.Left = Merge(left, right.Left);
             right.Left?.Parent = right;
             return right;
@@ -99,7 +105,8 @@ public class Treap<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, TreapNode<
 
     public override bool Remove(TKey key)
     {
-        if (Root == null || FindNode(key) == null) {
+        if (Root == null || FindNode(key) == null)
+        {
             return false;
         }
 
@@ -108,7 +115,8 @@ public class Treap<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, TreapNode<
         Root = Merge(lessNode, right);
         Root?.Parent = null;
         
-        if (deleteNode != null) {
+        if (deleteNode != null)
+        {
             this.Count--;
             OnNodeRemoved(null, null);
             return true;
