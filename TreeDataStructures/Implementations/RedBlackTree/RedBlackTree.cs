2d7faac2ddae1100b7b0ff54dfb1becef73f74cb
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2026-04-25 20:36:17 +0300

    Add method "SetRed" in RB

--- TreeDataStructures/Implementations/RedBlackTree/RedBlackTree.cs
+++ TreeDataStructures/Implementations/RedBlackTree/RedBlackTree.cs
@@ -5,7 +5,13 @@ namespace TreeDataStructures.Implementations.RedBlackTree;
 public class RedBlackTree<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, RbNode<TKey, TValue>>
 {
     private bool IsBlack(RbNode<TKey, TValue>? node) => node == null || node.Color == RbColor.Black;
+    
     private bool IsRed(RbNode<TKey, TValue>? node) => !IsBlack(node);
+
+    private void SetRed(RbNode<TKey, TValue>? node)
+    {
+        node?.Color = RbColor.Red;
+    }
 
     protected override RbNode<TKey, TValue> CreateNode(TKey key, TValue value) => new(key, value);
     
