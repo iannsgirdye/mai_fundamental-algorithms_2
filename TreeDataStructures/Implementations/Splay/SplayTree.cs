4014b0a72747c6079d0cf6e3650ea9d0add47402
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2026-05-01 22:59:10 +0300

    Add method "Splay" for SplayTree

--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -7,7 +7,54 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
 {
     protected override BstNode<TKey, TValue> CreateNode(TKey key, TValue value)
         => new(key, value);
-    
+
+    private void Splay(BstNode<TKey, TValue> node)
+    {
+        while (node.Parent != null)
+        {
+            if (node.Parent.IsLeftChild)
+            {
+                if (node.IsLeftChild)
+                {
+                    RotateRight(node.Parent.Parent);
+                    RotateRight(node.Parent);
+                }
+                else
+                {
+                    RotateLeft(node.Parent);
+                    RotateRight(node.Parent);
+                }
+                node = node.Parent.Parent;
+            }
+            else if (node.Parent.IsRightChild)
+            {
+                if (node.IsRightChild)
+                {
+                    RotateLeft(node.Parent.Parent);
+                    RotateLeft(node.Parent);
+                }
+                else
+                {
+                    RotateRight(node.Parent);
+                    RotateLeft(node.Parent);
+                }
+                node = node.Parent.Parent;
+            }
+            else  // node.Parent.Parent == null;
+            {
+                if (node.IsLeftChild)
+                {
+                    RotateRight(node.Parent);
+                }
+                else
+                {
+                    RotateLeft(node.Parent);
+                }
+                node = node.Parent;
+            }
+        }
+    }
+
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
         throw new NotImplementedException();
