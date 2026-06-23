cec1509d39547d2d096bf40a0d12db152070cab1
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2026-06-23 09:53:42 +0300

    Rename variables in FftMultiplier

--- Arithmetic/BigInt/MultiplyStrategy/FftMultiplier.cs
+++ Arithmetic/BigInt/MultiplyStrategy/FftMultiplier.cs
@@ -1,5 +1,5 @@
-﻿using System.Numerics;
-using Arithmetic.BigInt.Interfaces;
+﻿using Arithmetic.BigInt.Interfaces;
+using System.Numerics;
 
 namespace Arithmetic.BigInt.MultiplyStrategy;
 
@@ -8,73 +8,78 @@ internal class FftMultiplier : IMultiplier
     private static readonly int[] Mods = { 998244353, 1004535809, 469762049 };
     private const int PrimitiveRoot = 3;
 
-    public BetterBigInteger Multiply(BetterBigInteger a, BetterBigInteger b)
+    public BetterBigInteger Multiply(BetterBigInteger left, BetterBigInteger right)
     {
-        if (a.IsZero || b.IsZero)
-            return new BetterBigInteger([0], false);
+        var leftDigits = left.GetDigits();
+        var rightDigits = right.GetDigits();
 
-        var aDigits = a.GetDigits();
-        var bDigits = b.GetDigits();
+        int convolutionLength = leftDigits.Length + rightDigits.Length - 1;
 
-        int totalLen = aDigits.Length + bDigits.Length - 1;
-        int n = 1;
-        while (n < totalLen) n <<= 1;
+        int transformSize = 1;
+        while (transformSize < convolutionLength)
+            transformSize <<= 1;
 
-        int[,] residues = new int[Mods.Length, n];
-        for (int idx = 0; idx < Mods.Length; idx++)
+        int[,] remainders = new int[Mods.Length, transformSize];
+
+        for (int modulusIndex = 0; modulusIndex < Mods.Length; modulusIndex++)
         {
-            int mod = Mods[idx];
-            int[] fa = new int[n];
-            int[] fb = new int[n];
+            int modulus = Mods[modulusIndex];
 
-            for (int i = 0; i < aDigits.Length; i++) fa[i] = (int)(aDigits[i] % mod);
-            for (int i = 0; i < bDigits.Length; i++) fb[i] = (int)(bDigits[i] % mod);
+            int[] transformedLeft = new int[transformSize];
+            int[] transformedRight = new int[transformSize];
 
-            Ntt(fa, false, mod);
-            Ntt(fb, false, mod);
+            for (int i = 0; i < leftDigits.Length; i++)
+                transformedLeft[i] = (int)(leftDigits[i] % modulus);
+            for (int i = 0; i < rightDigits.Length; i++)
+                transformedRight[i] = (int)(rightDigits[i] % modulus);
 
-            for (int i = 0; i < n; i++)
-                fa[i] = (int)((long)fa[i] * fb[i] % mod);
+            NumberTheoreticTransform(transformedLeft, inverse: false, modulus);
+            NumberTheoreticTransform(transformedRight, inverse: false, modulus);
 
-            Ntt(fa, true, mod);
+            for (int i = 0; i < transformSize; i++)
+                transformedLeft[i] = (int)((long)transformedLeft[i] * transformedRight[i] % modulus);
 
-            for (int i = 0; i < n; i++)
-                residues[idx, i] = fa[i];
+            NumberTheoreticTransform(transformedLeft, inverse: true, modulus);
+
+            for (int i = 0; i < transformSize; i++)
+                remainders[modulusIndex, i] = transformedLeft[i];
         }
 
-        BigInteger[] coeffs = new BigInteger[totalLen];
-        BigInteger m1 = Mods[0], m2 = Mods[1], m3 = Mods[2];
-        BigInteger m1m2 = m1 * m2;
+        BigInteger[] convolutionCoefficients = new BigInteger[convolutionLength];
+        BigInteger modulus1 = Mods[0];
+        BigInteger modulus2 = Mods[1];
+        BigInteger modulus3 = Mods[2];
+        BigInteger productMods12 = modulus1 * modulus2;
 
-        for (int i = 0; i < totalLen; i++)
+        for (int i = 0; i < convolutionLength; i++)
         {
-            long r1 = residues[0, i];
-            long r2 = residues[1, i];
-            long r3 = residues[2, i];
+            long remainder1 = remainders[0, i];
+            long remainder2 = remainders[1, i];
+            long remainder3 = remainders[2, i];
 
-            long diff12 = (r2 - r1) % (long)m2;
-            if (diff12 < 0) diff12 += (long)m2;
-            long inv_m1_mod_m2 = ModInverse((long)(m1 % m2), (int)m2);
-            long t1 = diff12 * inv_m1_mod_m2 % (long)m2;
+            long difference12 = (remainder2 - remainder1) % (long)modulus2;
+            if (difference12 < 0) difference12 += (long)modulus2;
+            long inverseModulus1Mod2 = ModInverse((long)(modulus1 % modulus2), (int)modulus2);
+            long t1 = difference12 * inverseModulus1Mod2 % (long)modulus2;
 
-            BigInteger x12 = r1 + m1 * t1;
+            BigInteger x12 = remainder1 + modulus1 * t1;
 
-            long diff23 = (r3 - (long)(x12 % m3)) % (long)m3;
-            if (diff23 < 0) diff23 += (long)m3;
-            long m1m2_mod_m3 = (long)((m1 % m3) * (m2 % m3) % m3);
-            long inv_m1m2_mod_m3 = ModInverse(m1m2_mod_m3, (int)m3);
-            long t2 = diff23 * inv_m1m2_mod_m3 % (long)m3;
+            long difference23 = (remainder3 - (long)(x12 % modulus3)) % (long)modulus3;
+            if (difference23 < 0) difference23 += (long)modulus3;
+            long productMods12Mod3 = (long)((modulus1 % modulus3) * (modulus2 % modulus3) % modulus3);
+            long inverseProductMods12Mod3 = ModInverse(productMods12Mod3, (int)modulus3);
+            long t2 = difference23 * inverseProductMods12Mod3 % (long)modulus3;
 
-            coeffs[i] = x12 + m1m2 * t2;
+            convolutionCoefficients[i] = x12 + productMods12 * t2;
         }
 
         var resultDigits = new List<uint>();
         BigInteger carry = 0;
-        for (int i = 0; i < coeffs.Length; i++)
+        for (int i = 0; i < convolutionCoefficients.Length; i++)
         {
-            BigInteger cur = coeffs[i] + carry;
-            resultDigits.Add((uint)(cur & 0xFFFFFFFF));
-            carry = cur >> 32;
+            BigInteger currentValue = convolutionCoefficients[i] + carry;
+            resultDigits.Add((uint)(currentValue & 0xFFFFFFFF));
+            carry = currentValue >> 32;
         }
         while (carry > 0)
         {
@@ -83,73 +88,83 @@ internal class FftMultiplier : IMultiplier
         }
 
         int trim = resultDigits.Count;
-        while (trim > 0 && resultDigits[trim - 1] == 0) trim--;
-        if (trim == 0) return new BetterBigInteger([0], false);
+        while (trim > 0 && resultDigits[trim - 1] == 0)
+            trim--;
+        if (trim == 0)
+            return new BetterBigInteger([0], false);
         resultDigits.RemoveRange(trim, resultDigits.Count - trim);
 
-        bool negative = a.IsNegative ^ b.IsNegative;
+        bool negative = left.IsNegative ^ right.IsNegative;
         return new BetterBigInteger(resultDigits.ToArray(), negative);
     }
 
-    private void Ntt(int[] a, bool invert, int mod)
+    private void NumberTheoreticTransform(int[] array, bool inverse, int modulus)
     {
-        int n = a.Length;
-        for (int i = 1, j = 0; i < n; i++)
+        int length = array.Length;
+
+        for (int i = 1, j = 0; i < length; i++)
         {
-            int bit = n >> 1;
+            int bit = length >> 1;
             for (; (j & bit) != 0; bit >>= 1)
                 j ^= bit;
             j ^= bit;
             if (i < j)
-                (a[i], a[j]) = (a[j], a[i]);
+                (array[i], array[j]) = (array[j], array[i]);
         }
 
-        for (int len = 2; len <= n; len <<= 1)
+        for (int blockLength = 2; blockLength <= length; blockLength <<= 1)
         {
-            int wlen = PowMod(PrimitiveRoot, (mod - 1) / len, mod);
-            if (invert) wlen = ModInverse(wlen, mod);
+            int rootStep = PowMod(PrimitiveRoot, (modulus - 1) / blockLength, modulus);
+            if (inverse)
+                rootStep = ModInverse(rootStep, modulus);
 
-            for (int i = 0; i < n; i += len)
+            for (int start = 0; start < length; start += blockLength)
             {
-                long w = 1;
-                int half = len >> 1;
-                for (int j = 0; j < half; j++)
+                long currentRoot = 1;
+                int halfBlock = blockLength >> 1;
+
+                for (int offset = 0; offset < halfBlock; offset++)
                 {
-                    int u = a[i + j];
-                    int v = (int)(a[i + j + half] * w % mod);
-                    int x = u + v;
-                    if (x >= mod) x -= mod;
-                    int y = u - v;
-                    if (y < 0) y += mod;
-                    a[i + j] = x;
-                    a[i + j + half] = y;
-                    w = w * wlen % mod;
+                    int leftValue = array[start + offset];
+                    int rightValue = (int)(array[start + offset + halfBlock] * currentRoot % modulus);
+
+                    int sum = leftValue + rightValue;
+                    if (sum >= modulus) sum -= modulus;
+
+                    int difference = leftValue - rightValue;
+                    if (difference < 0) difference += modulus;
+
+                    array[start + offset] = sum;
+                    array[start + offset + halfBlock] = difference;
+
+                    currentRoot = currentRoot * rootStep % modulus;
                 }
             }
         }
 
-        if (invert)
+        if (inverse)
         {
-            int invN = ModInverse(n, mod);
-            for (int i = 0; i < n; i++)
-                a[i] = (int)((long)a[i] * invN % mod);
+            int inverseOfLength = ModInverse(length, modulus);
+            for (int i = 0; i < length; i++)
+                array[i] = (int)((long)array[i] * inverseOfLength % modulus);
         }
     }
 
-    private int PowMod(long a, long e, int mod)
+    private int PowMod(long baseValue, long exponent, int modulus)
     {
         long result = 1;
-        while (e > 0)
+        while (exponent > 0)
         {
-            if ((e & 1) == 1) result = result * a % mod;
-            a = a * a % mod;
-            e >>= 1;
+            if ((exponent & 1) == 1)
+                result = result * baseValue % modulus;
+            baseValue = baseValue * baseValue % modulus;
+            exponent >>= 1;
         }
         return (int)result;
     }
 
-    private int ModInverse(long a, int mod)
+    private int ModInverse(long value, int modulus)
     {
-        return PowMod(a, mod - 2, mod);
+        return PowMod(value, modulus - 2, modulus);
     }
 }
